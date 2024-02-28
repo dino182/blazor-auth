8f8e3512ab96b6553e25af46b7e1cebd9918524f
Dino Infantino <dino.infantino@displayplan.com>
2024-02-28 12:35:01 +0000

    Enforce authentication on all server resources

--- BlazorAuth/Server/Program.cs
+++ BlazorAuth/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +15,13 @@ builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefa
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-builder.Services.AddAuthorization();
+// Add a fallback authorisation policy that will be invoked by the static assets middleware
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 var app = builder.Build();
 
@@ -32,14 +39,16 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Static files are served after authentication and authorisation so that they are not publicly accessible
+// The authorisation fallback policy will prevent non-authenticated users accessing them
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+// Require authorisation by default on all endpoints
 app.MapRazorPages().RequireAuthorization();
 app.MapControllers().RequireAuthorization();
 app.MapFallbackToFile("index.html").RequireAuthorization();
